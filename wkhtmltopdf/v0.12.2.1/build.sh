92269a36a232221606eb622b9b8f69657c5aed2c
Jorgen Evens <jorgen@evens.eu>
2015-10-04 13:36:39 +0200

    [wkhtmltopdf] Add wkhtmltopdf.

---
+++ wkhtmltopdf/v0.12.2.1/build.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+
+set -e
+
+test -z "$NAME" && >&2 echo "Package NAME not set." && exit 1
+test -z "$VERSION" && >&2 echo "Version for $NAME not set." && exit 1
+
+OUT_DIR="$1"
+BUILD_DIR=`mktemp -d`
+VENDOR_DIR="/app/vendor"
+PREFIX="${VENDOR_DIR}/${NAME}"
+PACKAGE="${NAME}-${VERSION}"
+
+cd $BUILD_DIR
+VERSION_MAJOR="$(echo ${VERSION} | grep -P -o '\d+\.\d+' | head -n1)"
+curl -o "${PACKAGE}.deb" "http://download.gna.org/wkhtmltopdf/${VERSION_MAJOR}/${VERSION}/wkhtmltox-${VERSION}_linux-trusty-amd64.deb"
+dpkg -x "${PACKAGE}.deb" "${PACKAGE}"
+
+cd ${NAME}-*
+mkdir -p "${BUILD_DIR}${VENDOR_DIR}"
+mv "usr/local" "${BUILD_DIR}${PREFIX}"
+
+cd "${BUILD_DIR}${PREFIX}"
+rm -Rf share
+
+cd ..
+tar -caf "${OUT_DIR}/$PACKAGE.tar.gz" "$NAME"
+
+cat > "${OUT_DIR}/$PACKAGE.sh" << EOF
+#!/bin/sh
+
+unpack "\$INSTALLER_DIR/$PACKAGE.tar.gz" `md5sum $OUT_DIR/$PACKAGE.tar.gz | cut -d" " -f1`
+echo 'export LD_LIBRARY_PATH="\$LD_LIBRARY_PATH:/app/vendor/${NAME}/lib"' >> \${BUILD_DIR}/configure.sh
+echo 'export PATH="\$PATH:/app/vendor/${NAME}/bin"' >> \${BUILD_DIR}/configure.sh
+EOF
