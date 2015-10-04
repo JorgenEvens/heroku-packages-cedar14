4d7550e57a208e9b538ad86eaa21dc34f7905c3f
Jorgen Evens <jorgen@evens.eu>
2015-10-04 19:13:22 +0200

    [phantomjs] Added phantomjs 1.9.2

---
+++ phantomjs/v1.9.2/build.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+set -e
+
+test -z "$NAME" && >&2 echo "Package NAME not set." && exit 1
+test -z "$VERSION" && >&2 echo "Version for $NAME not set." && exit 1
+
+OUT_DIR="$1"
+BUILD_DIR=`mktemp -d`
+VENDOR_DIR="/app/vendor"
+PREFIX="${VENDOR_DIR}/${NAME}"
+PACKAGE="${NAME}-${VERSION}"
+
+cd $BUILD_DIR
+VERSION_MAJOR="$(echo ${VERSION} | grep -P -o '\d+\.\d+' | head -n1)"
+curl "https://phantomjs.googlecode.com/files/phantomjs-${VERSION}-linux-x86_64.tar.bz2" | tar -xj
+
+cd ${NAME}-*
+mkdir -p "${BUILD_DIR}${PREFIX}"
+mv "bin" "${BUILD_DIR}${PREFIX}/bin"
+
+cd "${BUILD_DIR}${VENDOR_DIR}"
+tar -caf "${OUT_DIR}/${PACKAGE}.tar.gz" "$NAME"
+
+cat > "${OUT_DIR}/$PACKAGE.sh" << EOF
+#!/bin/sh
+
+unpack "\$INSTALLER_DIR/$PACKAGE.tar.gz" `md5sum $OUT_DIR/$PACKAGE.tar.gz | cut -d" " -f1`
+echo 'export PATH="\$PATH:/app/vendor/${NAME}/bin"' >> \${BUILD_DIR}/configure.sh
+EOF
