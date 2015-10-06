7a9895371f1734e0f01766ef72a9d70031ae4d00
Jorgen Evens <jorgen@evens.eu>
2015-10-06 20:15:31 +0200

    [nodejs] Add 3 versions of nodejs
     - v0.10.40
     - v0.12.7
     - v4.1.2

---
+++ nodejs/v4.1.2/build.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+set -e
+
+test -z "$NAME" && >&2 echo "Package NAME not set." && exit 1
+test -z "$VERSION" && >&2 echo "Version for $NAME not set." && exit 1
+
+OUT_DIR="$1"
+BUILD_DIR=`mktemp -d`
+VENDOR_DIR="/app/vendor"
+PREFIX="${VENDOR_DIR}/${NAME}"
+PACKAGE="${NAME}-${VERSION}"
+
+cd $BUILD_DIR
+curl "https://nodejs.org/dist/v${VERSION}/node-v${VERSION}-linux-x64.tar.gz" | tar -xz
+
+cd node-*
+mkdir -p "${BUILD_DIR}${PREFIX}"
+mv "bin" "${BUILD_DIR}${PREFIX}/bin"
+mv "lib" "${BUILD_DIR}${PREFIX}/lib"
+mv "include" "${BUILD_DIR}${PREFIX}/include"
+
+cd "${BUILD_DIR}${VENDOR_DIR}"
+tar -caf "${OUT_DIR}/${PACKAGE}.tar.gz" "$NAME"
+
+cat > "${OUT_DIR}/$PACKAGE.sh" << EOF
+#!/bin/sh
+
+unpack "\$INSTALLER_DIR/$PACKAGE.tar.gz" `md5sum $OUT_DIR/$PACKAGE.tar.gz | cut -d" " -f1`
+echo 'export PATH="\$PATH:/app/vendor/${NAME}/bin"' >> \${BUILD_DIR}/configure.sh
+echo 'export LD_LIBRARY_PATH="\$LD_LIBRARY_PATH:/app/vendor/${NAME}/lib"' >> \${BUILD_DIR}/configure.sh
+EOF
